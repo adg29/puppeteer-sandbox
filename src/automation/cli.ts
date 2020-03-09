eed149803ccb78bf44d6230b63491b4136a944e3
Alan Garcia <alandavidgarcia@gmail.com>
2020-03-09 02:18:31 -0400

    Create cli.ts

---
+++ src/automation/cli.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import consola from 'consola';
+import { marketEtsyPerson } from '.';
+
+(async () => {
+  const { email, password, etsyPersonUsername} = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter email:',
+      default: process.env.ETSY_USERNAME
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Enter password:',
+      mask: '*',
+      default: process.env.ETSY_PASSWORD
+    },
+    {
+      type: 'input',
+      name: 'etsyPersonUsername',
+      message: 'Enter Etsy Person Username:',
+      default: 'alangalan'
+    }
+  ]);
+
+  try {
+    await marketEtsyPerson({ personUsername: etsyPersonUsername })({ email, password});
+  } catch (error) {
+    consola.error(error);
+    process.exitCode = 1;
+  }
+})();
